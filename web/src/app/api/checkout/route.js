cfa12f45c888e8bd5a5d5f8c1e417ed5c01f5dd6
Levente Gall <leventegall@proton.me>
2026-04-25 00:54:40 +0200

    Added Revolut Pay back to checkout methods

--- web/src/app/api/checkout/route.js
+++ web/src/app/api/checkout/route.js
@@ -40,7 +40,7 @@ export async function POST(req) {
     // Create Checkout Session
     const checkoutSession = await stripe.checkout.sessions.create({
       mode: "subscription",
-      payment_method_types: ["card", "paypal", "revolut_pay"],
+      payment_method_types: ["card", "revolut_pay"],
       line_items: [
         {
           price: priceId,
